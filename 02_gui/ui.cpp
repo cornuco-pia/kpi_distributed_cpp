e0e89ce417dc2bc29448fe83f1a88e7e415521f6
ivanna <trofymenkoivanna@outlook.com>
2025-09-09 10:52:40 +0300

    02: approx finished

---
+++ 02_gui/ui.cpp
@@ -0,0 +1,69 @@
+#include <QMainWindow>
+#include <QApplication>
+#include <QWidget>
+#include <QPainter>
+#include <QImage>
+#include <QResizeEvent>
+#include "myimage.h"
+
+
+static const uchar bitmap_height = 8;
+static const uchar bitmap_width = 8;
+static const uchar bitmap_data[8 * 8 * 3] = {
+    255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,
+    0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,
+    255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,
+    0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,
+    255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,
+    0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,
+    255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,
+    0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,   0,0,0,   255,255,255,
+};
+
+class BitmapWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    BitmapWidget(QWidget *parent = nullptr, bool use_image = false) : QWidget(parent) {
+        if (use_image == true){
+        // TODO: check what exactly is happening with channels
+        // fix rgba ?
+        image = QImage(image_data, image_width, image_height, image_width * 3, QImage::Format_RGB888);
+        }
+        else {
+        image = QImage(bitmap_data, bitmap_width, bitmap_height, QImage::Format_RGB888);
+        }
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        Q_UNUSED(event); // also possible to manually cast (void)event or skip the var name in arguments
+        QPainter painter(this);
+
+        QRect targetRect(0, 0, width(), height());
+
+        painter.drawImage(targetRect, image);
+    }
+
+    void resizeEvent(QResizeEvent *event) override {
+        QWidget::resizeEvent(event);
+        // QPainter handles scaling when drawing
+    }
+
+private:
+    QImage image;
+};
+
+int main(int argc, char *argv[]){
+    QApplication app(argc, argv);
+    
+    BitmapWidget checkerboard;
+    BitmapWidget image(nullptr, true);
+
+    checkerboard.show();
+    image.show();
+
+    return app.exec();
+}
+
+#include "ui.moc" // better to make a separate header file
---
+++ png_rgb_converter.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+# Open the PNG and convert to RGB (ignore alpha channel)
+img = Image.open("01_sysinfo/Civic_machine_assimilator.png").convert("RGB")
+width, height = img.size
+print("width, height:", width, height)
+pixels = list(img.getdata())
+
+with open("02_gui/myimage.h", "w") as f:
+    f.write(f"static const int image_width = {width};\n")
+    f.write(f"static const int image_height = {height};\n")
+    f.write("static const unsigned char image_data[] = {\n")
+
+    for i, (r, g, b) in enumerate(pixels):
+        f.write(f"0x{r:02X}, 0x{g:02X}, 0x{b:02X}, ")
+        if (i + 1) % 8 == 0:  # wrap lines
+            f.write("\n")
+
+    f.write("};\n")
